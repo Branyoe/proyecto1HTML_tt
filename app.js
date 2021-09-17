07b422204aa20aa05635b38393a769493a001fba
branyoe <bhernandez1@ucol.mx>
2021-09-16 20:13:13 -0500

    files modified and index finished

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.static("./public"));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/p1', (req, res) => {
+  res.sendFile(__dirname + '/public/p1.html');
+});
+
+app.get('/p2', (req, res) => {
+  res.sendFile(__dirname + '/public/p2.html');
+});
+
+app.get('/p3', (req, res) => {
+  res.sendFile(__dirname + '/public/p3.html');
+});
+
+app.listen(port, () => {
+  console.log('Server on port 3000');
+});
